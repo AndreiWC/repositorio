c17c692d64c6ed1b0deaeccdbd1f71ef44ea1b13
AndreiWC <andreiwelliton@gmail.com>
2018-04-12 22:16:32 -0300

    Arquivo inicial

---
+++ js/function.js
@@ -0,0 +1,16 @@
+
+function enviar(){
+    var nome = document.getElementById('nome').value;
+    var nick = document.getElementById('Nickname').value;
+    var senha = document.getElementById('senha').value;
+    var repita =document.getElementById('Repita').value;
+    var email = document.getElementById('email').value;
+    var data = document.getElementById('data').value;
+        
+               
+              
+                      alert("Você cadastrou os seguites dados " + nome + nick + senha + repita + 
+                      email + data );
+                    
+                
+ }
